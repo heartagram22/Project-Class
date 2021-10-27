6f3bfe54ac25f8b1d2bb711fbff4e9bcb4c70a19
Albin Gjonaj <92172913+heartagram22@users.noreply.github.com>
2021-10-27 17:36:29 +0200

    update

--- project/src/project/Project.java
+++ project/src/project/Project.java
@@ -1,21 +1,31 @@
-package project;
-
 public class Project {
-    public String name;
-    public String description;
-    public String name1(){
-        final String name = this.name;
-        return name;
+    private String name;
+    private String description;
+    private double initialCost = 0;
+    private static String defaultName = "Default NAME";
+    private static String defaultDesc = "Default DESC";
 
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getCost() { return initialCost; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public String ElevatorPitch() {
+        return String.format("%s (%02f) %s", name, initialCost, description);
     }
-    public Project(){
-    }
-    public Project(String name, String description){
+
+    public Project(String name, String description, double cost) {
         this.name = name;
+        this.initialCost = cost;
         this.description = description;
     }
-    public String getName () { return name;}
-    public void setName (String name){this.name = name;}
-    public String getDescription(){return description;}
-    public void setDescription(String this_procejt_sets_a_name){this.description = description;}
+    public Project(String name) {
+        this.name = name;
+        this.description = defaultDesc;
+    }
+    public Project() {
+        name = defaultName;
+        description = defaultDesc;
+    }
 }
